fa398b4d992dc145aebdcb6af40e3daf8a93f686
dicsii <84007219+Song-ded@users.noreply.github.com>
2023-05-20 16:49:09 +0300

    Add files via upload

--- cogs/messages.py
+++ cogs/messages.py
@@ -9,10 +9,15 @@ class Messages(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        print(message.author)
         if message.author.bot != True:
+            print(1)
             await self.db.create_table2()
+            print(2)
             await self.db.add_ustats(message.author)
+            print(3)
             await self.db.update_stats(message.author, 1)
+            print(4)
         else:
             pass
 
