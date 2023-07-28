d65774a6241907eb24fcab16e639ad4f14b2d26b
dicsii <84007219+Song-ded@users.noreply.github.com>
2023-07-28 18:32:08 +0300

    Update love.py

--- cogs/love.py
+++ cogs/love.py
@@ -14,25 +14,27 @@ class Lovepy(commands.Cog):
         user = await self.db.get_user(interaction.user)
         if member == interaction.user:
             await interaction.response.send_message('Все в порядке, но.. Вы указали самого себя😢', ephemeral=True)
-            break
-        if 100 <= user[1]:
-            print(f"member - {member}")
-            print(f"author - {interaction.user}")
-            print(f"text - {text}")
-            print("-----------------")
-            embed = disnake.Embed(title="Письмо✉️", description=text, color = 0x2ecc71)
-            embed.set_thumbnail(url=member.display_avatar.url)
-            try:
-                await member.send(embed=embed)
-                embed1 = disnake.Embed(title=f'Анонимное письмо - {member}', description="Письмо успешно отправлено!", color = 0x2ecc71)
-                embed1.set_thumbnail(url=member.display_avatar.url)
-                await interaction.response.send_message(embed=embed1, ephemeral=True)
-                await self.db.update_money(interaction.user, -100, 0)
-            except:
-                await interaction.response.send_message('Все в порядке, но.. У человека видимо закрыто ЛС😢', ephemeral=True)
+        elif member.bot:
+            await interaction.response.send_message('Все в порядке, но.. Вы указали бота😢', ephemeral=True)
         else:
-            embed = disnake.Embed(title=f'Анонимное письмо - {member}', description="У вас не хватает денег!", color=0xe74c3c)
-            embed.set_thumbnail(url=member.display_avatar.url)
-            await interaction.response.send_message(embed=embed, ephemeral=True)
+            if 100 <= user[1]:
+                print(f"member - {member}")
+                print(f"author - {interaction.user}")
+                print(f"text - {text}")
+                print("-----------------")
+                embed = disnake.Embed(title="Письмо✉️", description=text, color = 0x2ecc71)
+                embed.set_thumbnail(url=member.display_avatar.url)
+                try:
+                    await member.send(embed=embed)
+                    embed1 = disnake.Embed(title=f'Анонимное письмо - {member}', description="Письмо успешно отправлено!", color = 0x2ecc71)
+                    embed1.set_thumbnail(url=member.display_avatar.url)
+                    await interaction.response.send_message(embed=embed1, ephemeral=True)
+                    await self.db.update_money(interaction.user, -100, 0)
+                except:
+                    await interaction.response.send_message('Все в порядке, но.. У человека видимо закрыто ЛС😢', ephemeral=True)
+            else:
+                embed = disnake.Embed(title=f'Анонимное письмо - {member}', description="У вас не хватает денег!", color=0xe74c3c)
+                embed.set_thumbnail(url=member.display_avatar.url)
+                await interaction.response.send_message(embed=embed, ephemeral=True)
 def setup(bot):
     bot.add_cog(Lovepy(bot))
